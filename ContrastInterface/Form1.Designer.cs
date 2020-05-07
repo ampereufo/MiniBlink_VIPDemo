16a14a4f414bdd84a2734d55f2b7eaf08084c2d2
Ampere Ma <17136608@qq.com>
2020-05-07 14:46:39 +0800

    增加ContrastInterface

---
+++ ContrastInterface/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace ContrastInterface
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(10, 128);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MB.H有MBVIP_API.CS没有的";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(12, 156);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(410, 393);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox2.Location = new System.Drawing.Point(439, 156);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(410, 393);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(437, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(169, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "MBVIP_API.CS有MB.H没有的";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(159, 17);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(565, 21);
+            this.textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(159, 55);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(565, 21);
+            this.textBox4.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(68, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "MB.H文件目录：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(20, 59);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(137, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "MBVIP_API.CS文件目录：";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(752, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "对比";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(12, 102);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(837, 1);
+            this.panel1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(861, 562);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(877, 600);
+            this.MinimumSize = new System.Drawing.Size(877, 600);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MB.H和MBVIP_API.CS对比工具";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ ContrastInterface/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ContrastInterface
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox3.Text = Path.GetFullPath("..\\..\\..\\MiniBlink_VIPDLL\\mb.h");
+            textBox4.Text = Path.GetFullPath("..\\..\\..\\MiniBlink_VIPDLL\\MBVIP_API.cs");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+
+            List<string> MBFunNameList = new List<string>();
+            string[] strArrMB = File.ReadAllLines(textBox3.Text);
+            foreach (string str in strArrMB)
+            {
+                if (str.Length >= 8 && str.Substring(0, 8) == "ITERATOR")
+                {
+                    string strFunName = str.Split(',')[1].Replace(" ", "");
+                    MBFunNameList.Add(strFunName);
+                }
+
+                if (str.Length >= 7 && str.Substring(0, 7) == "inline ")
+                {
+                    string strFunName = str.Split(' ')[2];
+                    strFunName = strFunName.Substring(0, strFunName.LastIndexOf('('));
+                    MBFunNameList.Add(strFunName);
+                }
+            }
+
+            List<string> CSFunNameList = new List<string>();
+            string[] strArrCS = File.ReadAllLines(textBox4.Text);
+            foreach (string str in strArrCS)
+            {
+                if (str.Length >= 19 && str.Substring(0, 19) == "        [DllImport(")
+                {
+                    string strFunName = str.Split(',')[1].Replace(" EntryPoint = \"", "").Replace("\"", "");
+                    CSFunNameList.Add(strFunName);
+                }
+            }
+
+            foreach (string str in MBFunNameList)
+            {
+                if (!CSFunNameList.Contains(str))
+                {
+                    textBox1.AppendText(str + "\r\n\r\n");
+                }
+            }
+
+            foreach (string str in CSFunNameList)
+            {
+                if (!MBFunNameList.Contains(str))
+                {
+                    textBox2.AppendText(str + "\r\n\r\n");
+                }
+            }
+
+            if (textBox1.Text == string.Empty && textBox2.Text == string.Empty)
+            {
+                MessageBox.Show("没有找到新增或删除的接口", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
